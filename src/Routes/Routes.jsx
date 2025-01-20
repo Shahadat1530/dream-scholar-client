ea0fe5bb732c206a41a297be586134d7b5b2c615
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-20 17:15:36 +0600

    login page added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import Login from "../pages/Login-SignUp/Login";
 
 
 
@@ -7,6 +8,12 @@ const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout></MainLayout>,
+      children: [
+        {
+          path: 'login',
+          element: <Login></Login>
+        }
+      ]
     },
   ]);
 
