f37a6dd1e88d6f54507136533c1c8c33a618e233
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-23 20:36:44 +0600

    user dlt functionality added

--- src/pages/Dashboard/AdminDashboard/AllUsers.jsx
+++ src/pages/Dashboard/AdminDashboard/AllUsers.jsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useAxiosSecure from '../../hooks/useAxiosSecure';
 import { FaTrashAlt } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
@@ -13,6 +14,32 @@ const AllUsers = () => {
         }
     });
 
+    const handleDeleteUser = user => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+
+            }
+        });
+    };
 
     return (
         <div>
@@ -38,7 +65,7 @@ const AllUsers = () => {
                                         {/* role */}
                                     </td>
                                     <td>
-                                        <button className="btn btn-ghost btn-lg">
+                                        <button onClick={() => handleDeleteUser(user)} className="btn btn-ghost btn-lg">
                                             <FaTrashAlt className='text-red-600'></FaTrashAlt>
                                         </button>
                                     </td>
