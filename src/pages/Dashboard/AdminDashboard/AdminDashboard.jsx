96836da16971f17cdd0a375b86465999927b85b8
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-04-03 20:39:15 +0600

    fixing the color in Admin Dashboard

--- src/pages/Dashboard/AdminDashboard/AdminDashboard.jsx
+++ src/pages/Dashboard/AdminDashboard/AdminDashboard.jsx
@@ -11,21 +11,21 @@ const AdminDashboard = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div className="flex flex-col md:flex-row bg-gradient-to-r from-[#b91c1c] to-[#bffcfc] p-4 md:p-10 min-h-screen">
+        <div className="flex flex-col md:flex-row bg-primary p-4 md:p-10 min-h-screen">
             <Helmet>
                 <title>Admin Dashboard</title>
             </Helmet>
 
             {/* Mobile Menu Button */}
             <button
-                className="md:hidden text-white text-3xl mb-4 bg-[#b91c1c] p-2 rounded-md"
+                className="md:hidden text-white text-3xl mb-4 bg-accent] p-2 rounded-md"
                 onClick={() => setIsOpen(!isOpen)}
             >
                 <FaBars />
             </button>
 
             {/* Sidebar */}
-            <div className={`absolute md:static w-64 min-h-screen bg-[#b91c1c] p-5 text-white font-bold transform ${isOpen ? "translate-x-0" : "-translate-x-full"} md:translate-x-0 transition-transform duration-300 ease-in-out rounded-md md:rounded-none`}>
+            <div className={`absolute md:static w-64 min-h-screen bg-accent rounded-xl p-5 text-white font-bold transform ${isOpen ? "translate-x-0" : "-translate-x-full"} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
                 <ul className="space-y-4 md:text-lg">
                     <li>
                         <NavLink to='/adminDashboard/myProfile' className="flex items-center gap-2 hover:text-[#bffcfc]" onClick={() => setIsOpen(false)}>
@@ -67,7 +67,7 @@ const AdminDashboard = () => {
             </div>
 
             {/* Main Content */}
-            <div className="flex-1 p-6 md:p-10 bg-[#bffcfc] rounded-xl shadow-2xl w-full">
+            <div className="flex-1 p-6 md:p-10 bg-secondary rounded-xl shadow-2xl w-full">
                 <Outlet />
             </div>
         </div>
